6f7da7de855ead8a7a17a91dd0455115627542f2
Santiago Marquez betancur <santimb70@gmail.com>
2025-11-30 20:06:59 -0500

    changes finally

--- Apiwmovies/Repository/IRepository/ICategoryRepository.cs
+++ Apiwmovies/Repository/IRepository/ICategoryRepository.cs
@@ -4,14 +4,15 @@ namespace API.P.Movies.Repository.IRepository
 {
     public interface ICategoryRepository
     {
-        Task<ICollection<Category>> GetCategoriesAsync(); //Me retorna una lista de categorias
+        Task<ICollection<Category>> GetCategoriesAsync(); //retorna lista de categorias
         Task<Category> GetCategoryAsync(int id); //Me retorna una categoria por su Id
         Task<bool> CategoryExistsByIdAsync(int id); //Me dice si una categoria existe por su Id
         Task<bool> CategoryExistsByNameAsync(string name); //Me dice si una categoria existe por su nombre
         Task<bool> CreateCategoryAsync(Category category); //Me crea una categoria
         Task<bool> UpdateCategoryAsync(Category category); //Me actualiza una categoria, puedo actualizar el nombre y la fecha de actualizacion
         Task<bool> DeleteCategoryAsync(int id); //Me elimina una categoria
-        // Estas son las 7 firmas de los metodos
+        
+        //  son las 7 firmas de los metodos
     }
 }
 
--- Apiwmovies/Repository/IRepository/IMovieRepository.cs
+++ Apiwmovies/Repository/IRepository/IMovieRepository.cs
@@ -5,11 +5,11 @@ namespace API.P.Movies.Repository.IRepository
     public interface IMovieRepository
     {
         //7 firmas de los metodos
-        Task<ICollection<Movie>> GetMoviesAsync(); //Me retorna una lista de peliculas
-        Task<Movie> GetMovieAsync(int id); //Me retorna una pelicula por su Id
+        Task<ICollection<Movie>> GetMoviesAsync(); //retorna las peliculas 
+        Task<Movie> GetMovieAsync(int id); //retorna pelicula por id
         Task<bool> MovieExistsByIdAsync(int id); //Me dice si una pelicula existe por su Id
-        Task<bool> MovieExistsByNameAsync(string name); //Me dice si una pelicula existe por su nombre
-        Task<bool> CreateMovieAsync(Movie movie); //Me crea una pelicula
+        Task<bool> MovieExistsByNameAsync(string name); //si existe pelicula por su nombre
+        Task<bool> CreateMovieAsync(Movie movie); //crear pelicula
         Task<bool> UpdateMovieAsync(Movie movie); //Me actualiza una pelicula, puedo actualizar el nombre, duracion, descripcion, clasificacion y la fecha de actualizacion
         Task<bool> DeleteMovieAsync(int id); //Me elimina una pelicula
 
